3dc63480c1fbc157a752c3c5e2547e9ad1675d29
Angreh <angreh@gmail.com>
2016-07-18 17:52:06 -0300

    porque e quem somos

--- mktz/templates/home/porquecdc.php
+++ mktz/templates/home/porquecdc.php
@@ -1,16 +1,5 @@
 <div id="home-porquecdc">
     <div class="container">
-
-        <p>
-            A Casa do Corretor foi criada em 2003, para ser a melhor Corretora no segmento de benefícios de saúde suplementar do mercado. O investimento na formação de profissionais que atuam na consultoria e
-            comercialização de planos de assistência médica e odontológica rendeu frutos, pois
-            hoje somos a melhor corretora neste segmento. <br/>
-            <br/>
-            Seriedade, Responsabilidade e Credibilidade são nossos valores e os <br/>
-            corretores nosso patrimônio. <br/>
-            <br/>
-            Aqui todo profissional de vendas é bem vindo e terá uma Equipe <br/>
-            altamente capacitada para atendê-lo.
-        </p>
+        <div class="family"></div>
     </div><!-- .container -->
 </div><!-- #home-porquecdc -->
--- mktz/templates/home/quemsomos.php
+++ mktz/templates/home/quemsomos.php
@@ -1,5 +1,15 @@
 <div id="home-quemsomos">
     <div class="container">
-        quemsomos
+        <p>
+            A Casa do Corretor foi criada em 2003, para ser a melhor Corretora no segmento de benefícios de saúde suplementar do mercado. O investimento na formação de profissionais que atuam na consultoria e
+            comercialização de planos de assistência médica e odontológica rendeu frutos, pois
+            hoje somos a melhor corretora neste segmento. <br/>
+            <br/>
+            Seriedade, Responsabilidade e Credibilidade são nossos valores e os <br/>
+            corretores nosso patrimônio. <br/>
+            <br/>
+            Aqui todo profissional de vendas é bem vindo e terá uma Equipe <br/>
+            altamente capacitada para atendê-lo.
+        </p>
     </div><!-- .container -->
 </div><!-- #home-quemsomos -->
