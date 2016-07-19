538fe21d3a90bf4a69b7b807c394e9d02bb0bfa1
Angreh <angreh@gmail.com>
2016-07-19 08:21:54 -0300

    Logos e quem somos

--- mktz/templates/home/logos.php
+++ mktz/templates/home/logos.php
@@ -1,11 +1,14 @@
 <div id="home-logos">
     <div class="container">
 
-        <?php if ( have_posts() ) : the_post(); ?>
-
-            <?php the_content(); ?>
-
-        <?php endif; ?>
+        <ul>
+            <li><img src="/wp-content/themes/mktz_CDCorretor/assets/img/logos/allianz.png" alt="Allianz"></li>
+            <li><img src="/wp-content/themes/mktz_CDCorretor/assets/img/logos/amil.png" alt="Amil"></li>
+            <li><img src="/wp-content/themes/mktz_CDCorretor/assets/img/logos/bradesco.png" alt="Bradesco"></li>
+            <li><img src="/wp-content/themes/mktz_CDCorretor/assets/img/logos/notreDame.png" alt="NotreDame Intermédica"></li>
+            <li><img src="/wp-content/themes/mktz_CDCorretor/assets/img/logos/sulAmerica.png" alt="SulAmérica"></li>
+            <li><img src="/wp-content/themes/mktz_CDCorretor/assets/img/logos/one.png" alt="One Health"></li>
+        </ul>
 
     </div><!-- .container -->
 </div><!-- #home-logos -->
--- mktz/templates/home/quemsomos.php
+++ mktz/templates/home/quemsomos.php
@@ -1,5 +1,8 @@
 <div id="home-quemsomos">
     <div class="container">
+        <h3>
+            Quem <strong>Somos</strong>
+        </h3>
         <p>
             A Casa do Corretor foi criada em 2003, para ser a melhor Corretora no segmento de benefícios de saúde suplementar do mercado. O investimento na formação de profissionais que atuam na consultoria e
             comercialização de planos de assistência médica e odontológica rendeu frutos, pois
